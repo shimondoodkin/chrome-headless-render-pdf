3aba5e3d555bb601c9ca573087d811db172380a4
Shimon Doodkin <helpmepro1@gmail.com>
2017-12-10 04:20:20 +0200

    add image pdf code

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const CDP = require('chrome-remote-interface');
 const fs = require('fs');
+const os = require('os');
 const cp = require('child_process');
 const net = require('net');
 const commandExists = require('command-exists');
-
+//const Jimp = require('jimp')
+//const { createCanvas, Image } = require('canvas-prebuilt')
+const Canvas = require('canvas-prebuilt')
+var stream = require('stream');
+					
 class RenderPDF {
     constructor(options) {
         this.setOptions(options || {});
@@ -20,9 +25,19 @@ class RenderPDF {
             landscape: def('landscape', undefined),
             paperWidth: def('paperWidth', undefined),
             paperHeight: def('paperHeight', undefined),
-            includeBackground: def('includeBackground', undefined)
-        };
-
+            includeBackground: def('includeBackground', undefined),
+            captureFormat: def('captureFormat', undefined),
+            captureQuality: def('captureQuality', undefined),
+            dpi: def('dpi', undefined),
+            captureStepHeight: def('captureStepHeight', undefined),
+            saveFormat: def('saveFormat', undefined),
+            saveQuality: def('saveQuality', undefined),
+            clip: def('clip', undefined),
+            fromSurface: def('fromSurface', undefined),
+            timeout: def('timeout', undefined),
+            fullHeight: def('fullHeight', undefined),
+        }; 
+		
         this.commandLineOptions = {
             windowSize: def('windowSize', undefined),
             extraOptions: def('extraCliOptions', undefined)
@@ -79,13 +94,13 @@ class RenderPDF {
     async renderPdf(url, options) {
         return new Promise((resolve) => {
             CDP({port: this.port}, async (client) => {
-                this.log(`Opening ${url}`);
+                this.log(`Opening `+ url.substr(0,150)+(url.length>150?'...':'') );
                 const {Page, Emulation, Animation} = client;
                 await Page.enable();
-                await Animation.enable();
+                //await Animation.enable();
 
                 await Page.navigate({url});
-                await Emulation.setVirtualTimePolicy({policy: 'pauseIfNetworkFetchesPending', budget: 5000});
+                await Emulation.setVirtualTimePolicy({policy: 'pauseIfNetworkFetchesPending', budget: options.timeout||5000});
 
                 const loaded = this.cbToPromise(Page.loadEventFired);
                 const jsDone = this.cbToPromise(Emulation.virtualTimeBudgetExpired);
@@ -105,7 +120,153 @@ class RenderPDF {
             });
         });
     }
+	
+	delay(ms) {
+		return new Promise((resolve)=> {
+			setTimeout(resolve, ms);
+		});
+	}
+	
+    async renderCapture(url, options, script) {
+        return new Promise((resolve) => {
+            CDP({port: this.port}, async (client) => {
+				this.log(`Opening `+ url.substr(0,150)+(url.length>150?'...':'') );
+                const {Page, Emulation, Animation, Runtime /*, HeadlessExperimental*/} = client;
+                await Page.enable();
+                await Animation.enable();
+				//await HeadlessExperimental.enable() 
+                await Page.navigate({url});
+				await Emulation.setVirtualTimePolicy({policy: 'pauseIfNetworkFetchesPending', budget: options.timeout||5000}); 
+                const loaded = this.cbToPromise(Page.loadEventFired);
+                const jsDone = this.cbToPromise(Emulation.virtualTimeBudgetExpired);
+                //const shotDone = this.cbToPromise(HeadlessExperimental.mainFrameReadyForScreenshots); 
+				
+				if(script) 
+					await this.profileScope('Running script', async () => {
+						await Runtime.evaluate({expression:script})
+					});	
+					
+                if(url!=='about:blank')
+					await this.profileScope('Wait for load', async () => {
+						await loaded;
+					});
+					
+                await this.profileScope('Wait for js execution', async () => {
+                    await jsDone;
+                });
+				//await Runtime.evaluate({expression:'window.scrollTo(\'0px\', \'600px\');'})
+				//this.log('Openin 8')
+				//await this.delay(4000)
+                //await this.profileScope('Wait for shot Done', async () => {
+                //    await shotDone;
+                //}); 
+                //const png = await HeadlessExperimental.beginFrame({format:'png'});
+				
+				let tile_height=this.commandLineOptions.windowSize[1];
+				//let tiles=[];
+				let height_captured = 0
+				
+				var maxheight=options.clip.height;
+				var saveFormat=options.saveFormat||'png';
+				var saveQuality=options.saveQuality||98; // 98 is max quality that has some compression
+				var dpi=options.dpi||96; // modern  (not high end) dispalys are at 96 dpi, and dpi of css pixel is also 96
+				var dpiwidth=Math.round(options.clip.width*(72/dpi));
+				var dpiheight=Math.round(maxheight*(72/dpi));
+				var captureStepHeight=options.captureStepHeight;
+				
+				var pageWidth  = options.paperWidth  ? Math.floor(options.paperWidth  * 72) : dpiwidth  ; // pdfs are always at 72 dpi
+				var pageHeight = options.paperHeight ? Math.floor(options.paperHeight * 72) : dpiheight ;
+				//this.log( options.paperWidth+" x "+options.paperHeight+' = ' + pageWidth+" x "+pageHeight  )
+ 
+				const options2 = {};
+				if(options.captureFormat !== undefined)    options2.format      = options.captureFormat;
+				if(options.captureQuality !== undefined)   options2.quality     = options.captureQuality;
+				if(options.fromSurface !== undefined)      options2.fromSurface = options.fromSurface;
+				if(options.clip !== undefined)             options2.clip        = options.clip;
+				
+				//var pageimg = new Jimp(options.clip.width, maxheight);
+				const canvas = new Canvas(options.clip.width,maxheight)
+				const ctx = canvas.getContext('2d')
+				
+				let tile_y=0;
+				await this.profileScope('full captureScreenshot', async () => {
+					while (height_captured < maxheight ){
+					  await this.profileScope('partial captureScreenshot', async () => {
+						  options2.clip.height=(maxheight-height_captured>tile_height)?tile_height: (maxheight-height_captured)		
+						  options2.clip.y=tile_y*tile_height;
+						  
+						  
+						  await Runtime.evaluate({expression:'window.scrollTo(\'0px\', \''+options2.clip.y+'\');'})
+						  const png  = await Page.captureScreenshot(options2); 
 
+						  const buff = Buffer.from(png.data, 'base64');
+						  
+						  //tiles.push(buff);
+						  
+						  //let img = await Jimp.read(buff);
+						  let img = new Canvas.Image();
+						  img.src=buff;
+						  //pageimg.blit( img, 0, height_captured );
+						  ctx.drawImage(img, 0, height_captured);
+
+						  height_captured += tile_height
+						  tile_y++;
+					  });
+					}
+				});
+                client.close();
+				//pageimg.quality( 100 ); 
+				//pageimg.getBuffer(Jimp.MIME_PNG,(err,buf2)=>resolve(buf2) );
+				await this.profileScope('making save format', ()=>{ 
+					return (new Promise((resolve)=>{
+						if(options.saveFormat==='png'||options.saveFormat==='pngpdf'||options.saveFormat===undefined)
+						{						
+							canvas.toBuffer((err, buf2)=>{
+								resolve( buf2 );
+							}); // CanvasPNG Buffer
+						}
+						else if(options.saveFormat==='jpeg'||options.saveFormat==='jpegpdf')
+						{
+							let chunks = []; // We'll store all the data inside this array
+							const stream = canvas.jpegStream({
+								bufsize: 4096 // output buffer size in bytes, default: 4096
+							  , quality: saveQuality // JPEG quality (0-100) default: 75
+							  , progressive: false // true for progressive compression, default: false
+							});
+							stream.on('data', function(chunk){
+							  chunks.push(chunk);
+							});
+							stream.on('end', function(){
+							  const buf2 = Buffer.concat(chunks);
+							  resolve( buf2 );
+							});
+						}
+						else 
+							throw new Error('incorrect saveFormat specified')
+					})).then( (buf2)=> {
+							if(options.saveFormat==='pngpdf'||options.saveFormat==='jpegpdf')
+							{
+								const canvas = new Canvas( pageWidth , pageHeight,'pdf')	
+								const ctx = canvas.getContext('2d')
+								const img = new Canvas.Image();
+								img.src=buf2;
+								//pageimg.blit( img, 0, 0 );
+								ctx.drawImage(img, 0, 0 ,dpiwidth,dpiheight);
+								const buf3 = canvas.toBuffer(); 
+								resolve( buf3 );
+							}
+							else
+								resolve( buf2 );
+					})
+				});
+				
+					
+				
+				
+            });
+        });
+    }
+	
     generatePdfOptions() {
         const options = {};
         if (this.options.landscape !== undefined) {
@@ -130,6 +291,98 @@ class RenderPDF {
         if(this.options.paperHeight !== undefined) {
             options.paperHeight = parseFloat(this.options.paperHeight);
         }
+		
+        if(this.options.timeout !== undefined) {
+            options.timeout = Math.round(parseFloat(this.options.timeout));
+        }
+        return options;
+    }
+	
+	
+    generateCaptureOptions() {
+        const options = {};
+
+		
+        if(this.options.captureFormat !== undefined) {
+			     if( this.options.captureFormat.match(/png/i)      ) options.captureFormat = 'png';
+			else if( this.options.captureFormat.match(/jpg|jpeg/i) ) options.captureFormat = 'jpeg';
+        }
+		
+        if(this.options.captureQuality !== undefined) {
+            let quality = Math.round(parseFloat(this.options.captureQuality));
+			if(quality>100)quality=100;
+			if(quality<0)quality=0;
+			options.captureQuality=quality;
+        }
+		
+        if(this.options.saveFormat !== undefined) {
+			let format=this.options.saveFormat;
+			     if( format.match(/jpgpdf|jpegpdf/i) ) options.saveFormat = 'jpegpdf';
+			else if( format.match(/pngpdf/i) ) options.saveFormat = 'pngpdf';
+			else if( format.match(/png/i) ) options.saveFormat = 'png';
+			else if( format.match(/jpg|jpeg/i) ) options.saveFormat = 'jpeg';
+        }
+		
+        if(this.options.saveQuality !== undefined) {
+            let quality = Math.round(parseFloat(this.options.saveQuality));
+			if(quality>100)quality=100;
+			if(quality<0)quality=0;
+			options.saveQuality=quality;
+        }
+		
+		
+        if(this.options.dpi !== undefined) {
+            options.dpi = parseFloat(this.options.dpi);
+        }
+        if(this.options.captureStepHeight !== undefined) {
+            options.captureStepHeight = parseFloat(this.options.captureStepHeight);
+        }
+
+        if(this.options.paperWidth !== undefined) {
+            options.paperWidth = parseFloat(this.options.paperWidth);
+        }
+
+        if(this.options.paperHeight !== undefined) {
+            options.paperHeight = parseFloat(this.options.paperHeight);
+        }
+		
+        if (this.options.fromSurface !== undefined) {
+            options.fromSurface = !!this.options.fromSurface;
+        }
+ 
+		if (this.options.clip) {
+			let  x=0
+				,y=0
+				,width=1600
+				,height=1200
+				,scale=1;
+				
+			if(this.options.clip.x !== undefined) {
+				x = parseFloat(this.options.clip.x)
+			}
+			if(this.options.clip.width !== undefined) {
+				width = parseFloat(this.options.clip.width)
+			}
+			if(this.options.clip.height !== undefined) {
+				height = parseFloat(this.options.clip.height)
+			}
+			if(this.options.clip.scale !== undefined) {
+				scale = parseFloat(this.options.clip.scale)
+			}
+			
+			options.clip={
+				 x
+				,y
+				,width
+				,height
+				,scale
+			};
+        }
+		
+        if(this.options.timeout !== undefined) {
+            options.timeout = Math.round(parseFloat(this.options.timeout));
+        }
+		
         return options;
     }
 
@@ -165,13 +418,14 @@ class RenderPDF {
     }
 
     browserLog(type, msg) {
-            this.log(`(chrome) (${type}) `,msg);
+            this.log(`(chrome) (${type}) `,msg.constructor.name==='Socket'?'Socket':msg);
      
     }
 
     async spawnChrome() {
         const chromeExec = this.options.chromeBinary || await this.detectChrome();
         this.log('Using', chromeExec);
+		var isWin = /^win/.test(process.platform);
         const commandLineOptions = [
 		     '--headless', 
              '--no-sandbox', 
@@ -187,10 +441,10 @@ class RenderPDF {
              '--disable-plugins', 
              '--disable-sync', 
              '--incognito',
-             '--disk-cache-dir=/dev/null',
+             isWin?'--disk-cache-dir=null':'--disk-cache-dir=/dev/null',
              `--remote-debugging-port=${this.port}`, 
              '--disable-gpu'
-            ];
+            ]; 
 
         if (this.commandLineOptions.windowSize !== undefined ) {
           commandLineOptions.push(`--window-size=${this.commandLineOptions.windowSize[0]},${this.commandLineOptions.windowSize[1]}`);
